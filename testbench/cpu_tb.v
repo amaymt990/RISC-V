9ac05eae1a31f0227ffe9787df8a3e142a77f1f2
Amay M Thamban <amaythamban990@gmail.com>
2026-07-22 17:40:13 +0530

    Complete single-cycle RISC-V CPU with arithmetic, memory, branch and jump support

--- testbench/cpu_tb.v
+++ testbench/cpu_tb.v
@@ -31,7 +31,6 @@ initial begin
     $display("x1 = %d", uut.rf.registers[1]);
     $display("x2 = %d", uut.rf.registers[2]);
     $display("x3 = %d", uut.rf.registers[3]);
-    $display("x4 = %d", uut.rf.registers[4]);
 
 $finish;
 end
